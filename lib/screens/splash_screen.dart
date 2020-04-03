481def863b377fb2fce7b8b1aa8950783c6c9272
Jin <it.minhhung@gmail.com>
2020-04-03 16:16:14 +0700

    splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:startup_namer/main.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    fetchSomething(context);
+
+    return Container(
+      alignment: Alignment.center,
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          stops: [0.1, 0.9],
+          colors: [
+            Color(0xFFFC5C7D),
+            Color(0xFF6A82FB),
+          ],
+        ),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 90.0),
+      child: Image.asset(
+        'assets/news-placeholder.png',
+        fit: BoxFit.scaleDown,
+      ),
+    );
+  }
+
+  void fetchSomething(BuildContext context) async {
+    print("start background job....");
+    await new Future.delayed(const Duration(seconds: 2));
+    print("background job completed!!!");
+
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  // void fetchSomething() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   final _isHaveData = prefs.getString(Constants.sp_have_data) ?? "";
+
+  //   //TODO Call API from server and do sth
+  //   await new Future.delayed(const Duration(seconds: 2));
+
+  //   if (_isHaveData.isEmpty) {
+  //     await prefs.setString(Constants.sp_have_data, "just login");
+  //     view.goToNextScreen(true);
+  //   } else {
+  //     view.goToNextScreen(false);
+  //   }
+  // }
+
+  //  void goToNextScreen(context, bool shouldLogin) {
+  //   if (shouldLogin) {
+  //     Navigator.pushReplacementNamed(context, Constants.login_screen);
+  //   } else {
+  //     Navigator.pushReplacementNamed(context, Constants.main_screen);
+  //   }
+  // }
+
+}
